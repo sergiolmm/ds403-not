9dae52579952bc9a26e0115ab01b79ca43b0866c
sergiolmm <sergio@unicamp.br>
2023-08-23 21:28:11 -0300

    aula 4 primeiro material APP

--- aula3/lib/main.dart
+++ aula3/lib/main.dart
@@ -7,15 +7,24 @@ void main() {
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Row(
+    return Container(
+      padding: const EdgeInsets.all(2.0),
+      alignment: Alignment.center,
+      //  transform: Matrix4.rotationZ(0.1),
+      color: Colors.blueAccent,
+      width: 320.0,
+      height: 320.0 ,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        textDirection: TextDirection.ltr,
         children: [
-          Icon(Icons.ac_unit, color: Colors.deepOrange),
-          Icon(Icons.accessibility_outlined, color: Colors.lightGreenAccent,),
-          Icon(Icons.icecream_rounded, color: Colors.red,)
+          Text("slo", textDirection: TextDirection.ltr,),
+         // Icon(Icons.accessibility_outlined, color: Colors.lightGreenAccent,),
+         // Icon(Icons.icecream_rounded, color: Colors.red,)
         ],
       ),
     );
+
   }
 
 }
